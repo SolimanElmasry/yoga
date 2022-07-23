7e0a3b7611c674eea06ffe39f71baaa6cc54e669
soliman elmasry <solyana38@gmail.com>
2022-07-23 10:54:26 +0300

    u

---
+++ main.js
@@ -0,0 +1,36 @@
+let button1=document.getElementById('b1');
+button1.onclick =function(e){
+    // e.preventDefault();
+    button1.style.borderColor='#025a4e';
+    button2.style.borderColor='#ff000000';
+    button3.style.borderColor='#ff000000';
+    button4.style.borderColor='#ff000000';
+    button1.style.letterSpacing='2px';
+};
+let button2=document.getElementById('b2');
+button2.onclick =function(e){
+    // e.preventDefault();
+    button1.style.borderColor='#ff000000';
+    button2.style.borderColor='#025a4e';
+    button3.style.borderColor='#ff000000';
+    button4.style.borderColor='#ff000000';
+    button2.style.letterSpacing='2px';
+};
+let button3=document.getElementById('b3');
+button3.onclick =function(e){
+    // e.preventDefault();
+    button1.style.borderColor='#ff000000';
+    button2.style.borderColor='#ff000000';
+    button3.style.borderColor='#025a4e';
+    button4.style.borderColor='#ff000000';
+    button3.style.letterSpacing='2px';
+};
+let button4=document.getElementById('b4');
+button4.onclick =function(e){
+    // e.preventDefault();
+    button1.style.borderColor='#ff000000';
+    button2.style.borderColor='#ff000000';
+    button3.style.borderColor='#ff000000';
+    button4.style.borderColor='##025a4e';
+    button4.style.letterSpacing='2px';
+};
